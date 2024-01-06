1175237afba776bcd1fce34692f5e957b11f7bf3
Thu Trang Le <lthutrang17@gmail.com>
2024-01-06 15:11:04 +0700

    preview

--- src/app/courses/[id]/_components/CoursePreview/index.tsx
+++ src/app/courses/[id]/_components/CoursePreview/index.tsx
@@ -1,34 +1,22 @@
-import { Card, CardHeader, Divider, CardBody, CardFooter, Image, Link } from "@nextui-org/react"
 import React from "react"
+import CourseSideBar from "./CourseSideBar"
 const CoursePreview = () => {
-    return ( <Card className="max-w-[400px]">
-        <CardHeader className="flex gap-3">
-            <Image
-                alt="nextui logo"
-                height={40}
-                radius="sm"
-                src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-                width={40}
-            />
-            <div className="flex flex-col">
-                <p className="text-md">NextUI</p>
-                <p className="text-small text-default-500">nextui.org</p>
+    return ( 
+        <div>
+            <div>
+
             </div>
-        </CardHeader>
-        <Divider/>
-        <CardBody className="p-5">
-            <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-        <Divider/>
-        <CardFooter>
-            <Link
-                isExternal
-                showAnchorIcon
-                href="https://github.com/nextui-org/nextui"
-            >
-        Visit source code on GitHub.
-            </Link>
-        </CardFooter>
-    </Card>)
+       
+            <div className="grid grid-cols-3 gap-4 ">
+                <div className="col-span-2 "><h1>main</h1></div>
+            
+                <div>
+                    <CourseSideBar/>
+                </div>
+            </div>
+        </div>
+        
+ 
+    )
 }
 export default CoursePreview
