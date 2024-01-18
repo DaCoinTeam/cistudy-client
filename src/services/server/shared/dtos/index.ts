3721d6fe47801748b334906cb06cae0275929c31
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-01-19 00:28:45 +0700

    123

--- src/services/server/shared/dtos/index.ts
+++ src/services/server/shared/dtos/index.ts
@@ -2,7 +2,7 @@
 import PostDto from "./post.dto"
 import PostContentDto from "./post-content.dto"
 import UserDto from "./user.dto"
-import Response from "./response.dto"
+import Response from "../interfaces/response.interface"
 import CourseDto from "./course.dto"
 
 export type { PostDto, PostContentDto, UserDto, CourseDto, Response }
