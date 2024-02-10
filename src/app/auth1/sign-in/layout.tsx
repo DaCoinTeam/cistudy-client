f00cf349dd8e9bdf3635acef4b7668c08276b9d7
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2024-02-10 23:12:04 +0700

    123123

--- src/app/auth1/sign-in/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import React from "react"
-import { FormikProviders } from "./_hooks"
-import { ContextProps } from "@app/_shared"
-
-const RootLayout = (props: ContextProps) => {
-    return <FormikProviders>{props.children}</FormikProviders>
-}
-export default RootLayout
